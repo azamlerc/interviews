0db7a12ed8045b59eff9257589be4b303e7e2a0e
Andrew Zamler-Carhart <azc@mac.com>
2019-11-12 20:51:47 -0500

    Refactored using let. Made combine a one liner.

--- fourfours/fourfours.clj
+++ fourfours/fourfours.clj
@@ -7,27 +7,31 @@
 
 (defn round [d] (Double/parseDouble (format "%.6f" d)))
 
+(defn div [a b] (if (zero? b) [] (round (/ a b))))
+
+(defn both-ways [op a b] [(op a b) (op b a)])
+
 (defn combine [fa fb]
-  (->> (for [a fa b fb] 
-    [(+ a b) (- a b) (- b a) (* a b)
-      (if (zero? b) [] (round (/ a b)))
-      (if (zero? a) [] (round (/ b a)))])
-  (flatten)
-  (distinct)
-  (sort)))
-	
+  (->> (for [a fa b fb op [+ - * div]] (both-ways op a b))
+    (flatten)
+    (distinct)
+    (sort)))
+
 (defn four-fours []
-  (def f1 [0.4 2.0 4.0 24.0])
-  (def f2 (combine f1 f1))
-  (def f3 (combine f1 f2))
-  (def f4 (set (concat (combine f1 f3) (combine f2 f2))))
-  (def found (filter #(contains? f4 (double %)) (range 1 101)))
-  (def missing (filter #(not (contains? f4 (double %))) (range 1 101)))
-  (println "1 four" f1) (println)
-  (println "2 fours" f2) (println)
-  (println "3 fours" f3) (println)
-  (println "4 fours" found) (println)
-  (println "missing" missing) (println)
-  (if (= missing [73 77 81 87 93 99]) (println "Success!")))
+  (let [
+    f1 [0.4 2.0 4.0 24.0]
+    f2 (combine f1 f1)
+    f3 (combine f1 f2)
+    f4 (set (concat (combine f1 f3) (combine f2 f2)))
+    target (range 1 101)
+    found (filter #(contains? f4 (double %)) target)
+    missing (filter #(not (contains? (set found) %)) target)]
+  (println
+    "1 four" f1 "\n\n"
+    "2 fours" f2 "\n\n"
+    "3 fours" f3 "\n\n"
+    "4 fours" found "\n\n"
+    "missing" missing "\n")
+  (if (= missing [73 77 81 87 93 99]) (println "Success!"))))
 
 (four-fours)
