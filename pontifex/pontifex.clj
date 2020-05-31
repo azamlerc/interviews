d5599ab620fa60d5c553333cdb282b8e102a0759
Andrew Zamler-Carhart <azc@mac.com>
2020-05-31 17:16:03 +0200

    Linting.

--- pontifex/pontifex.clj
+++ pontifex/pontifex.clj
@@ -1,14 +1,19 @@
-;; This program implements the Solitaire encryption algorithm as described in the appendix of Cryptonomicon by Neal Stephenson: https://www.schneier.com/academic/solitaire/
+;; This program implements the Solitaire encryption algorithm as described in
+;; the appendix of the book Cryptonomicon by Neal Stephenson:
 
-;; Solitaire is a cyper that can be performed using a deck of playing cards. If two people each have a deck of playing cards in the same order, each can generate a keystream that can be used to encypher and decypher a message.
+;; https://www.schneier.com/academic/solitaire/
+
+;; Solitaire is a cyper that can be performed using a deck of playing cards.
+;; If two people each have a deck of playing cards in the same order, each
+;; can generate a keystream that can be used to encypher and decypher a message.
 
 ;; Basic Functions
 
 (defn blocks [string] (clojure.string/join " "
   (map #(apply str %1) (partition 5 string))))
 
-(defn modulo [value base] (if (< value 1) 
-  (modulo (+ value 26) base) 
+(defn modulo [value base] (if (< value 1)
+  (modulo (+ value 26) base)
   (+ (mod (- value 1) base) 1)))
 
 (defn letter [number] (char (+ (modulo number 26) 64)))
@@ -39,10 +44,9 @@
 (defn aaaaa [length] (apply str (repeat length "A")))
 
 (defn example [length] (subs (apply str (repeat (int (Math/ceil (/ length 10))) "KDWUPONOWT")) 0 length))
-  
-(defn encrypt [plain generator] (let [string (pretty plain)] 
-  (addStrings string (generator (count string)))))
 
+(defn encrypt [plain generator] (def string (pretty plain))
+  (addStrings string (generator (count string))))
 (defn decrypt [encrypted generator]
   (subtractStrings encrypted (generator (count encrypted))))
 
@@ -52,12 +56,12 @@
 (def jokerA [1 :joker])
 (def jokerB [2 :joker])
 
-(defn makeDeck [] (concat 
+(defn makeDeck [] (concat
   (for [suit suits number (range 1 14)] [number suit])
   [jokerA jokerB]))
 
 (defn cardNumber [card] (let [rank (first card) suit (last card)]
-  (case suit 
+  (case suit
   :clubs rank
   :diamonds (+ rank 13)
   :hearts (+ rank 26)
@@ -65,16 +69,16 @@
   :joker 53)))
 
 (defn cardLetter [card] (let [rank (first card) suit (last card)]
-  (case suit 
+  (case suit
   :clubs (letter rank)
   :diamonds (letter (+ rank 13))
   :hearts (letter rank)
   :spades (letter (+ rank 13))
   :joker nil)))
 
-(defn insert [array index item] (let [coll (vec array)] 
+(defn insert [array index item] (let [coll (vec array)]
   (concat (conj (subvec coll 0 index) item) (subvec coll index))))
-(defn removeAt [array index] (let [coll (vec array)] 
+(defn removeAt [array index] (let [coll (vec array)]
   (concat (subvec coll 0 index) (subvec coll (inc index)))))
 
 (defn moveTo [card newIndex deck] (let [
@@ -88,8 +92,8 @@
   newIndex (+ (mod (- (+ oldIndex down) 1) 53) 1)]
   (insert tempDeck newIndex card)))
 
-(defn cut [index deck] (let [d (vec deck)] 
-  (concat (subvec d index 53) 
+(defn cut [index deck] (let [d (vec deck)]
+  (concat (subvec d index 53)
           (subvec d 0 index)
           (subvec d 53 54))))
 
@@ -98,9 +102,9 @@
   indexA (.indexOf deck cardA)
   indexB (.indexOf deck cardB)
   index1 (min indexA indexB)
-  index2 (max indexA indexB)] 
-  (concat (subvec d (inc index2) 54) 
-          (subvec d index1 (inc index2)) 
+  index2 (max indexA indexB)]
+  (concat (subvec d (inc index2) 54)
+          (subvec d index1 (inc index2))
           (subvec d 0 index1))))
 
 (defn play [deck] (def shuffled (->> deck
@@ -110,12 +114,12 @@
   (cut (cardNumber (last shuffled)) shuffled))
 
 (defn keystream [length string deck] (let [
-  newDeck (play deck)                                           
+  newDeck (play deck)
   number (cardNumber (first newDeck))
-  letter (cardLetter (nth newDeck number))] 
+  letter (cardLetter (nth newDeck number))]
   (if (nil? letter)
     (keystream length string newDeck)
-    (if (> length 0) 
+    (if (> length 0)
       (keystream (dec length) (str string letter) newDeck)
       string))))
 
@@ -124,34 +128,34 @@
 (defn solitaire [length, phrase] (keystream length "" (keyDeck phrase (makeDeck))))
 
 (def tests [
-  {:plain "Hello!" 
-   :pretty "HELLO" 
-   :encrypted "IFMMP" 
+  {:plain "Hello!"
+   :pretty "HELLO"
+   :encrypted "IFMMP"
    :generator aaaaa}
-  {:plain "Do not use PC!" 
-   :pretty "DONOT USEPC" 
-   :encrypted "OSKJJ JGTMW" 
+  {:plain "Do not use PC!"
+   :pretty "DONOT USEPC"
+   :encrypted "OSKJJ JGTMW"
    :generator example}
-  {:plain "Aaaaaaaaaa!" 
-   :pretty "AAAAA AAAAA" 
-   :encrypted "EXKYI ZSGEH" 
+  {:plain "Aaaaaaaaaa!"
+   :pretty "AAAAA AAAAA"
+   :encrypted "EXKYI ZSGEH"
    :generator #(solitaire %1 "")}
-  {:plain "Aaaaaaaaaaaaaaa!" 
-   :pretty "AAAAA AAAAA AAAAA" 
-   :encrypted "ITHZU JIWGR FARMW" 
+  {:plain "Aaaaaaaaaaaaaaa!"
+   :pretty "AAAAA AAAAA AAAAA"
+   :encrypted "ITHZU JIWGR FARMW"
    :generator #(solitaire %1 "FOO")}
-  {:plain "Solitaire!" 
-   :pretty "SOLIT AIREX" 
-   :encrypted "KIRAK SFJAN" 
+  {:plain "Solitaire!"
+   :pretty "SOLIT AIREX"
+   :encrypted "KIRAK SFJAN"
    :generator #(solitaire %1 "CRYPTONOMICON")}])
 
 (doseq [item tests]
-  (assert (= (blocks (pretty (:plain item))) 
+  (assert (= (blocks (pretty (:plain item)))
              (:pretty item)) (str "pretty " (:plain item)))
-  (assert (= (blocks (encrypt (:plain item) (:generator item))) 
+  (assert (= (blocks (encrypt (:plain item) (:generator item)))
              (:encrypted item)) (str "encrypted " (:plain item)))
   (assert (= (blocks (decrypt (encrypt (:plain item)
                (:generator item)) (:generator item)))
              (:pretty item)) (str "decrypted " (:plain item))))
-  
+
 (println "All tests passed!")
