efa1d476fead45d30b9b5209f93ecf063bbdfef6
Andrew Zamler-Carhart <azc@mac.com>
2020-06-23 22:50:36 +0200

    Cleaned up instance variables.

--- pontifex/pontifex.rb
+++ pontifex/pontifex.rb
@@ -1,16 +1,16 @@
-# This program implements the Solitaire encryption algorithm 
-# as described in the appendix of the book Cryptonomicon by 
+# This program implements the Solitaire encryption algorithm
+# as described in the appendix of the book Cryptonomicon by
 # Neal Stephenson:
 
 # https://www.schneier.com/academic/solitaire/
 
-# Solitaire is a cypher that can be performed using a deck of 
-# playing cards. If two people each have a deck of playing 
-# cards in the same order, each can generate a keystream that 
+# Solitaire is a cypher that can be performed using a deck of
+# playing cards. If two people each have a deck of playing
+# cards in the same order, each can generate a keystream that
 # can be used to encypher and decypher a message.
 
 class Integer
-  def charValue() 
+  def charValue()
     value = self
     while value < 1
       value += 26
@@ -21,31 +21,31 @@ class Integer
 end
 
 class String
-  def intValue() 
+  def intValue()
     return self.ord - 64
   end
-  
+
   def plus(c)
     return (self.intValue + c.intValue).charValue
   end
-  
+
   def minus(c)
     return (self.intValue - c.intValue).charValue
   end
-  
+
   def add(s)
     return self.split("").zip(s.split(""))
       .map { |pair| pair[0].plus(pair[1]) }
       .join
   end
-  
+
   def subtract(s)
     return self.split("").zip(s.split(""))
       .map { |pair| pair[0].minus(pair[1]) }
       .join
   end
-  
-  def blocks() 
+
+  def blocks()
     return self.scan(/.{5}/).join(" ")
   end
 end
@@ -53,15 +53,15 @@ end
 class Card
   attr_accessor :suit
   attr_accessor :rank
-  
+
   def initialize(suit, rank)
     @suit = suit
     @rank = rank
   end
-  
+
   def to_s
     return rank_str() + suit_str()
-  end  
+  end
 
   def rank_str()
     if suit == :joker
@@ -81,7 +81,7 @@ class Card
       end
     end
   end
-    
+
   def suit_str()
     case suit
     when :clubs
@@ -97,7 +97,7 @@ class Card
     end
   end
 
-  def number() 
+  def number()
     case suit
     when :clubs
       return rank
@@ -107,7 +107,7 @@ class Card
       return rank + 26
     when :spades
       return rank + 39
-    else 
+    else
       return 53
     end
   end
@@ -122,8 +122,8 @@ class Card
       return nil
     end
   end
-end 
-  
+end
+
 class Deck
   attr_accessor :cards
   attr_accessor :jokerA
@@ -133,26 +133,26 @@ class Deck
     @cards = []
     [:clubs, :diamonds, :hearts, :spades].each do |suit|
       13.times do |rank|
-        cards.push(Card.new(suit, rank + 1))
+        @cards.push(Card.new(suit, rank + 1))
       end
     end
     @jokerA = Card.new(:joker, 1)
     @jokerB = Card.new(:joker, 2)
-    cards.push(jokerA)
-    cards.push(jokerB)
+    @cards.push(@jokerA)
+    @cards.push(@jokerB)
   end
-  
-  def move(card, downBy) 
+
+  def move(card, downBy)
     index = cards.index(card)
-    cards.delete(card)
+    @cards.delete(card)
     newIndex = (index + downBy - 1) % cards.count + 1
-    cards.insert(newIndex, card)
+    @cards.insert(newIndex, card)
   end
-  
+
   def cut(count)
-    @cards = cards[count...53] + 
-             cards[0...count] +
-             cards[53...54]
+    @cards = @cards[count...53] +
+             @cards[0...count] +
+             @cards[53...54]
   end
 
   def tripleCut(card1, card2)
@@ -161,11 +161,11 @@ class Deck
     if index1 > index2
       index1, index2 = index2, index1
     end
-    @cards = cards[(index2 + 1)...cards.count] + 
-             cards[index1...(index2 + 1)] +
-             cards[0, index1]
+    @cards = @cards[(index2 + 1)...cards.count] +
+             @cards[index1...(index2 + 1)] +
+             @cards[0, index1]
     end
-end 
+end
 
 class Generator
   def keystream(length)
@@ -175,69 +175,65 @@ class Generator
     end
     return stream[0..(length - 1)]
   end
-  
-  def more() 
+
+  def more()
     return "A"
-  end  
+  end
 end
 
 class Example < Generator
-  def more() 
+  def more()
     return "KDWUPONOWT"
-  end  
+  end
 end
 
 class Solitaire < Generator
-  attr_accessor :deck
-  
   def initialize(passphrase)
     @deck = Deck.new
-    
-    if passphrase != nil 
+
+    if passphrase != nil
       passphrase.upcase.split("").each do |letter|
         shuffle()
-        deck.cut(letter.intValue)
+        @deck.cut(letter.intValue)
       end
     end
   end
-  
-  def more() 
+
+  def more()
     shuffle()
-    number = deck.cards.first.number
-    letter = deck.cards[number].letter
-    if letter == nil 
+    number = @deck.cards.first.number
+    letter = @deck.cards[number].letter
+    if letter == nil
       return more()
     end
     return letter
   end
 
   def shuffle()
-    deck.move(deck.jokerA, 1)
-    deck.move(deck.jokerB, 2)
-    deck.tripleCut(deck.jokerA, deck.jokerB)
-    deck.cut(deck.cards.last.number)
+    @deck.move(@deck.jokerA, 1)
+    @deck.move(@deck.jokerB, 2)
+    @deck.tripleCut(@deck.jokerA, @deck.jokerB)
+    @deck.cut(@deck.cards.last.number)
   end
 end
 
 class Crypt
-  attr_accessor :generator
-  
   def initialize(generator)
     @generator = generator
   end
-  
+
   def encrypt(string)
     value = Crypt.encryptable(string)
-    keystream = generator.keystream(value.length)
+    keystream = @generator.keystream(value.length)
     return value.add(keystream)
   end
-  
+
   def decrypt(string)
     value = string.gsub(" ", "")
-    keystream = generator.keystream(value.length)
+    keystream = @generator.keystream(value.length)
     return value.subtract(keystream)
   end
-  
+
   def self.encryptable(string)
     value = string.upcase.gsub(/[^A-Z]/i, '')
     while value.length % 5 > 0
@@ -248,11 +244,6 @@ class Crypt
 end
 
 class Test
-  attr_accessor :plain
-  attr_accessor :pretty
-  attr_accessor :encrypted
-  attr_accessor :generator
-  
   def initialize(plain, pretty, encrypted, generator)
     @plain = plain
     @pretty = pretty
@@ -261,39 +252,28 @@ class Test
   end
 
   def assert(type, a, b)
-    puts type + ": " + (a == b ? 
-      "pass (" + a + ")" : 
+    puts type + ": " + (a == b ?
+      "pass (" + a + ")" :
       "fail (" + a + " / " + b + ")")
   end
-  
+
   def run()
-    crypt = Crypt.new(generator.call)
-    assert("pretty ", Crypt.encryptable(plain).blocks, pretty)
-    assert("encrypt", crypt.encrypt(plain).blocks, encrypted)
-    crypt = Crypt.new(generator.call)
-    assert("decrypt", crypt.decrypt(encrypted).blocks, pretty)
+    crypt = Crypt.new(@generator.call)
+    assert("pretty ", Crypt.encryptable(@plain).blocks, @pretty)
+    assert("encrypt", crypt.encrypt(@plain).blocks, @encrypted)
+    crypt = Crypt.new(@generator.call)
+    assert("decrypt", crypt.decrypt(@encrypted).blocks, @pretty)
   end
 end
 
-tests = [
-  Test.new("Hello!", 
-       "HELLO", 
-       "IFMMP", 
-       Proc.new { Generator.new }),
-  Test.new("Do not use PC!", 
-       "DONOT USEPC", 
-       "OSKJJ JGTMW", 
-       Proc.new { Example.new }),
-  Test.new("Aaaaaaaaaa!", 
-       "AAAAA AAAAA", 
-       "EXKYI ZSGEH",
-       Proc.new { Solitaire.new(nil) }),
-  Test.new("Aaaaaaaaaaaaaaa!", 
-       "AAAAA AAAAA AAAAA", 
-       "ITHZU JIWGR FARMW", 
-       Proc.new { Solitaire.new("FOO") }),
-  Test.new("Solitaire!", 
-       "SOLIT AIREX", 
-       "KIRAK SFJAN", 
-       Proc.new { Solitaire.new("CRYPTONOMICON") })
+[ Test.new("Hello!", "HELLO", "IFMMP",
+    Proc.new { Generator.new }),
+  Test.new("Do not use PC!", "DONOT USEPC", "OSKJJ JGTMW",
+    Proc.new { Example.new }),
+  Test.new("Aaaaaaaaaa!", "AAAAA AAAAA", "EXKYI ZSGEH",
+    Proc.new { Solitaire.new(nil) }),
+  Test.new("Aaaaaaaaaaaaaaa!", "AAAAA AAAAA AAAAA", "ITHZU JIWGR FARMW",
+    Proc.new { Solitaire.new("FOO") }),
+  Test.new("Solitaire!", "SOLIT AIREX", "KIRAK SFJAN",
+    Proc.new { Solitaire.new("CRYPTONOMICON") })
 ].each { |test| test.run }
