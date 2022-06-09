ecfccad13fcf98989c6323039895069866e0cc57
Andrew Zamler-Carhart <azc@mac.com>
2022-06-08 22:13:24 -0400

    Cleaned up date code.

--- subscriptions/subscriptions.js
+++ subscriptions/subscriptions.js
@@ -35,12 +35,12 @@ while (date.getFullYear() == 2022) {
     if (user.active) {
       if (chance(0.005)) {
         user.active = false;
-        user.end = new Date(date.getTime());
+        user.end = date.getTime();
       }
     } else {
       if (!user.start && chance(0.03)) {
         user.active = true;
-        user.start = new Date(date.getTime());
+        user.start = date.getTime();
       }
     }
   }
@@ -58,10 +58,10 @@ let total = 0;
 date = new Date(2022, 0, 1);
 while (date.getFullYear() == 2022) {
   for (let user of users) {
-    if (user.start && user.start.getTime() == date.getTime()) {
+    if (user.start == date.getTime()) {
       total++;
     }
-    if (user.end && user.end.getTime() == date.getTime()) {
+    if (user.end == date.getTime()) {
       total--;
     }   
   }
@@ -73,3 +73,6 @@ while (date.getFullYear() == 2022) {
 if (dailyTotals.join(' ') == myDailyTotals.join(' ')) {
   console.log('Success!');
 }
+
+// Estension: remove to the check for !user.start to allow
+// users to re-subscribe. How does that affect your solution?
